31e1d84540005deaa57ee1f2345a1675e6053bb1
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-10-23 15:36:56 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+int data = 0;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(22, INPUT);
+}
+
+void loop()
+{
+  int sensor = digitalRead(22);
+  Serial.print("DATA SENSOR :");
+  Serial.println(sensor);
+
+  // put your main code here, to run repeatedly:
+  if (sensor == 0)
+  {
+    if (data == 10)
+    {
+      Serial.print("SUDAH MELEBIHI BATAS");
+    }
+    else
+    {
+      data = data + 1;
+    }
+  }
+  Serial.println(data);
+
+  delay(500);
+}
